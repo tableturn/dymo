689e904cd0ac1022af070761b6dce3b612513933
Pierre Martin <hickscorp@gmail.com>
2018-08-28 16:24:37 +0100

    Adds application configuration files.

---
+++ config/dev.exs
@@ -0,0 +1,13 @@
+use Mix.Config
+
+# The repo configuration should be taken from the environment in priority.
+config :dymo,
+       Dymo.Repo,
+       adapter: Ecto.Adapters.Postgres,
+       pool_size: 10,
+       hostname: System.get_env("POSTGRES_HOST") || "localhost",
+       username: System.get_env("POSTGRES_USER") || "postgres",
+       password: System.get_env("POSTGRES_PASSWORD") || "postgres",
+       database: System.get_env("POSTGRES_DB") || "dymo_dev"
+
+config :dymo, :repo, Dymo.Repo
---
+++ config/test.exs
@@ -0,0 +1,13 @@
+use Mix.Config
+
+# The repo configuration should be taken from the environment in priority.
+config :dymo,
+       Dymo.Repo,
+       adapter: Ecto.Adapters.Postgres,
+       pool: Ecto.Adapters.SQL.Sandbox,
+       hostname: System.get_env("POSTGRES_HOST") || "localhost",
+       username: System.get_env("POSTGRES_USER") || "postgres",
+       password: System.get_env("POSTGRES_PASSWORD") || "postgres",
+       database: System.get_env("POSTGRES_DB") || "dymo_test"
+
+config :dymo, :repo, Dymo.Repo
