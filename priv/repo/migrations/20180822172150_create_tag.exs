dcef2be8f441e3699722c3ca91da35efc9e699d7
Pierre Martin <hickscorp@gmail.com>
2018-08-28 23:22:10 +0100

    Removes Dymo prefix from migration module.

---
+++ priv/repo/migrations/20180822172150_create_tag.exs
@@ -0,0 +1,13 @@
+defmodule Repo.Migrations.CreateTags do
+  use Ecto.Migration
+
+  def change do
+    create table(:tags) do
+      add :label, :string, null: false
+
+      timestamps()
+    end
+
+    create index(:tags, [:label], unique: true)
+  end
+end
