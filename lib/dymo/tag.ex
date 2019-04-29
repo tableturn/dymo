3335c46f6ad66c764fde35a647226ce358cb5f0d
Jean Parpaillon <jean.parpaillon@free.fr>
2019-04-29 17:53:23 +0200

    Add `Tag.match_ns?/2` function

--- lib/dymo/tag.ex
+++ lib/dymo/tag.ex
@@ -13,6 +13,9 @@ defmodule Dymo.Tag do
 
   @typedoc "Defines simple tags identified by a unique label."
   @type t :: %__MODULE__{}
+  @typedoc "Defines a namespace for tag"
+  @type ns :: nil | atom | [atom]
+
   schema "tags" do
     # Regular fields.
     field(:label, :string)
@@ -68,5 +71,47 @@ defmodule Dymo.Tag do
       %{id: nil} -> Dymo.repo().get_by!(@me, label: label)
       tag -> tag
     end
+  end
+
+  @doc """
+  Return true if tag is in given namespace
+
+  ## Examples
+
+      iex> match_ns?(%Tag{label: "t1"}, nil)
+      true
+
+      iex> match_ns?(%Tag{label: "t1"}, :ns1)
+      false
+
+      iex> match_ns?(%Tag{label: "ns1:t1"}, :ns1)
+      true
+
+      iex> match_ns?(%Tag{label: "ns1:t1"}, :ns2)
+      false
+
+      iex> match_ns?(%Tag{label: "ns1"}, :ns1)
+      false
+
+      iex> match_ns?(%Tag{label: "ns1:ns2:t1"}, [:ns1, :ns2])
+      true
+
+      iex> match_ns?(%Tag{label: "ns1:ns2"}, [:ns1, :ns2])
+      false
+
+      iex> match_ns?(%Tag{label: "ns1:ns2:ns3:t1"}, [:ns1, :ns2])
+      true
+  """
+  @spec match_ns?(t, ns) :: boolean
+  def match_ns?(_t, nil), do: true
+
+  def match_ns?(%{label: label}, ns) do
+    prefix =
+      ns
+      |> List.wrap()
+      |> Enum.join(":")
+      |> Kernel.<>(":")
+
+    String.starts_with?(label, prefix)
   end
 end
