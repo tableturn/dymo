36c0f4eaefda0b2f1d7f27aa6f030ccbad7367a7
Pierre Martin <hickscorp@gmail.com>
2020-01-03 10:39:28 +0000

    Prevents Dymo.repo() to be set prematurelly.

--- lib/dymo.ex
+++ lib/dymo.ex
@@ -1,8 +1,6 @@
 defmodule Dymo do
   @moduledoc false
 
-  @repo Application.get_env(:dymo, :ecto_repo)
-
   @spec repo() :: module
-  def repo, do: @repo
+  def repo, do: :dymo |> Application.get_env(:ecto_repo)
 end
